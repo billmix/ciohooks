e6c1c860f965f286c887088062a9d7b9e3c5224d
billmix <designer.bill@gmail.com>
2019-02-12 12:08:52 -0800

    verbose body with moment

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var moment = require('moment');
 const siteid = process.env.SITEID
 const apikey = process.env.APIKEY
 
@@ -14,7 +15,27 @@ exports.cioWrite = function cioWrite (req, res) {
   var options = {
     method: 'PUT',
     uri:    'https://' + siteid + ':' + apikey + '@track.customer.io/api/v1/customers/' + req.body.email,
-    json:    req.body
+    json:    true,
+    body: {
+      "accepts_marketing" : req.body.addresses,
+      "addresses" : req.body.addresses,
+      "created_at" : moment().unix,
+      "currency" : req.body.currency,
+      "default_address" : req.body.default_address,
+      "email" : req.body.email,
+      "first_name" : req.body.first_name,
+      "id" : req.body.id,
+      "last_name" : req.body.last_name,
+      "note" : req.body.note,
+      "orders_count" : req.body.orders_count,
+      "phone" : req.body.phone,
+      "state" : req.body.state,
+      "tax_exempt" : req.body.tax_exempt,
+      "test" : req.body.test,
+      "total_spent" : req.body.total_spent,
+      "updated_at" : req.body.updated_at,
+      "verified_email" : req.body.verified_email,
+    },
   };
 
   request(options, function(error, presp, pbody){
